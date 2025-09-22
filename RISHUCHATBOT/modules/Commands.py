102b7173ed13f1ed2668fddb7e6cc79479b8c0a8
MisteryDevs <officialbotdev@gmail.com>
2025-09-22 11:33:33 +0530

    Update Commands.py

--- RISHUCHATBOT/modules/Commands.py
+++ RISHUCHATBOT/modules/Commands.py
@@ -1,4 +1,3 @@
-
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from RISHUCHATBOT import RISHUCHATBOT as app
@@ -7,5 +6,8 @@ from RISHUCHATBOT import RISHUCHATBOT as app
 async def react_to_messages(client: Client, message: Message):
     try:
         await message.react("👍")
+        print(f"Reacted to {message.from_user.first_name}")
     except Exception as e:
         print(f"Failed to react to message: {e}")
+
+
